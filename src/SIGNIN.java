464960363bdf9958724db613be186f5fd47fbf2e
Christian Mamani Choque <christianmamani.dam@gmail.com>
2024-05-22 12:22:13 +0200

    antes de crear la rama para trabajar con SQL Server

--- src/SIGNIN.java
+++ src/SIGNIN.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+
 public class SIGNIN {
+    private JPanel SIGN_IN_PANEL;
+    private JPanel HEADER_PANEL;
+    private JLabel CLINICA_LABEL;
+    private JPanel MAIN_PANEL;
+
 
 }
